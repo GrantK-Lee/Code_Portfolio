4b5e5f95cd0b046a1ebd1583469daeaed89a5fd7
Grant K Lee <39206400+GrantK-Lee@users.noreply.github.com>
2022-09-14 08:55:05 -0700

    Create FreelanceGoalType.cs

---
+++ Aquifer_PE_Sample/.NET/Models.Domain/FreelanceGoalType.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquiferPE.Models.Domain.Resumes
+{
+    public class FreelanceGoalType
+    {
+        public int Id { get; set; }
+        public string FreelanceGoal { get; set; }
+    }
+}
