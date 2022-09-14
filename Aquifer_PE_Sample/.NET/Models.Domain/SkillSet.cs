d7dfa10c2e0f36d83ab1096c1edc788da6fd4340
Grant K Lee <39206400+GrantK-Lee@users.noreply.github.com>
2022-09-14 08:56:15 -0700

    Create SkillSet.cs

---
+++ Aquifer_PE_Sample/.NET/Models.Domain/SkillSet.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquiferPE.Models.Domain.Resumes
+{
+    public class SkillSet
+    {
+        public int Id { get; set; }
+        public string Skill { get; set; }
+    }
+}
